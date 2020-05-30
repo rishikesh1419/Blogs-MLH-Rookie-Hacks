bac4179037032c9e0fa1fae4db98cfb52469c8cd
vhvidhani <vhvidhani12@gmail.com>
2020-05-30 19:55:59 +0000

    Added DB

--- app.js
+++ app.js
@@ -7,7 +7,24 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-mongoose.connect("mongodb://localhost/blogs")
+mongoose.connect("mongodb://localhost/blogs_app")
+
+var blogSchema = new mongoose.Schema(
+    {
+        title: String,
+        image: String,
+        body: String,
+        created: {type: Date, default: Date.now}
+    }
+);
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+Blog.create({
+    title: "Test 1",
+    image: "https://source.unsplash.com/collection/8610070/600x400",
+    body: "First blog"
+});
 
 app.get("/", (req, res) => {
     res.send("Initialized!");
